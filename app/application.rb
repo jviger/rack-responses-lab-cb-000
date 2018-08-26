997e22ac57ede7caef9f5f13b75575eceea78772
Jason Viger <jason.viger@gmail.com>
2018-08-26 08:10:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
-    current_time = Time.new
+    current_time = Time.hour(14)
     if current_time.hour < 12
     resp.write "Good Morning!"
     else
